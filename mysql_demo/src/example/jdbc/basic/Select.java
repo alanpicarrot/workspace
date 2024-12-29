2cc466369580d9bf813a17484b298f4d016dd945
Chang Yu-hasiang <alanpicarrot@gmail.com>
2024-12-29 15:07:44 +0800

    JDBC註解

--- mysql_demo/src/example/jdbc/basic/Select.java
+++ mysql_demo/src/example/jdbc/basic/Select.java
@@ -8,15 +8,18 @@ import java.sql.Statement;
 
 public class Select {
 	public static void main(String[] args) {
-		String url = "jdbc:mysql://localhost:3306/demo_db";
+		//設定連線資訊（url:port，db名稱，user名稱，密碼）
+		String url = "jdbc:mysql://localhost:3306/homework_db";
 		String user = "root";
 		String password = "123456";
 
 		try (Connection conn = DriverManager.getConnection(url, user, password)) {
 			// 搜尋資料
 			String searchSQL = """
-
-					""";
+					SELECT * FROM member_info
+					WHERE account = 'mem002' AND password = 'pass5678';
+										""";
+			//透過連線建立一個Statement物件，接收我們想要實際執行的SQL語法
 			try (Statement stmt = conn.createStatement(); ResultSet rs = stmt.executeQuery(searchSQL)) {
 				while (rs.next()) {
 					System.out.println("Member ID: " + rs.getInt("member_id"));
@@ -30,8 +33,9 @@ public class Select {
 
 			// 1號會員的登入操作
 			String loginSQL = """
-
-					""";
+					SELECT * FROM member_info
+					WHERE account = 'mem001' AND password = 'pass1234';
+										""";
 			try (Statement stmt = conn.createStatement(); ResultSet rs = stmt.executeQuery(loginSQL)) {
 				while (rs.next()) {
 					System.out.println("Member ID: " + rs.getInt("member_id"));
--- mysql_demo/src/example/jdbc/basic/Update.java
+++ mysql_demo/src/example/jdbc/basic/Update.java
@@ -7,23 +7,27 @@ import java.sql.Statement;
 
 public class Update {
 	public static void main(String[] args) {
-		String url = "jdbc:mysql://localhost:3306/demo_db";
+		String url = "jdbc:mysql://localhost:3306/homework_db";
 		String user = "root";
 		String password = "123456";
 
 		try (Connection conn = DriverManager.getConnection(url, user, password)) {
 			// 修改1號會員的密碼
 			String updatePasswordSQL = """
-
-					""";
+					UPDATE member_info
+					SET password = 'ji394mysql'
+					WHERE member_id = 1;
+										""";
 			try (Statement stmt = conn.createStatement()) {
 				stmt.executeUpdate(updatePasswordSQL);
 			}
 
 			// 修改3號會員的名稱及電子信箱
 			String updateMemberSQL = """
-
-					""";
+					UPDATE member_info
+					SET member_name = 'Vic Liu', email = 'vic.liu@example.com'
+					WHERE member_id = 3;
+										""";
 			try (Statement stmt = conn.createStatement()) {
 				stmt.executeUpdate(updateMemberSQL);
 			}
